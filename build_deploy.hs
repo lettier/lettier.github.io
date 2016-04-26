fc445de38412d5b2581cba0537d1965afdb87fdc
Lettier <contact@lettier.com>
2016-04-26 01:13:06 -0400

    Adds type signatures to build and deploy code.

--- build_deploy.hs
+++ build_deploy.hs
@@ -5,55 +5,78 @@
 
 {-# LANGUAGE OverloadedStrings #-}
 
-import Data.Text (pack, concat, isSuffixOf)
-import Turtle (pwd, format, fp, empty, realpath, rmtree, shell, testdir)
+import Data.Text (pack, concat, isSuffixOf, Text, intercalate)
+import Turtle (pwd, format, fp, empty, realpath, rmtree, shell, testdir, ExitCode, FilePath)
 import Control.Monad (when)
 
+main :: IO ()
 main = do
   cwd <- pwd
   let filePath = format fp cwd
-  when ("lettier.github.io" `isSuffixOf` filePath) $
-    do
-      buildAndPush
-      deploy
-      return ()
-
+  when ("lettier.github.io" `isSuffixOf` filePath) $ do
+    buildAndPush
+    deploy
+    return ()
   return ()
 
+buildAndPush :: IO ExitCode
 buildAndPush = do
   rmSiteTree
   cabalBuild
-  pushToHakyll
+  pushToHakyllBranch
 
+deploy :: IO ExitCode
 deploy = do
-  checkoutMaster
-  checkoutHakyllSiteTree
+  checkoutMasterBranch
+  checkoutHakyllBranchSiteTree
   rmImagesTree
   rmPostsTree
   rmCssTree
   rsyncHakyllSiteTree
   rmSiteTree
   pushToMaster
-  checkoutHakyll
+  checkoutHakyllBranch
 
 -- Build && Push
 
+rmSiteTree :: IO ()
 rmSiteTree = rmTree "./_site"
 
-cabalBuild = shellEmpty "cabal clean && cabal configure && cabal build -j && cabal run rebuild -j"
+cabalBuild :: IO ExitCode
+cabalBuild = shellEmpty $ combineCommands [clean, configure, build, rebuild]
+  where clean = "cabal clean "
+        configure = "cabal configure"
+        build = "cabal build -j"
+        rebuild = "cabal rebuild -j"
 
-pushToHakyll = shellEmpty "git add -u && git add -A && git commit --amen && git push -f"
+pushToHakyllBranch :: IO ExitCode
+pushToHakyllBranch = shellEmpty $ combineCommands [
+    update
+    , commit
+    , push
+  ]
+  where update = "git add -A"
+        commit = "git commit"
+        push = "git push"
 
 -- Deploy
 
-checkoutMaster = shellEmpty "git checkout master"
+checkoutMasterBranch :: IO ExitCode
+checkoutMasterBranch = shellEmpty "git checkout master"
 
-checkoutHakyllSiteTree = shellEmpty "git checkout hakyll _site"
+checkoutHakyllBranchSiteTree :: IO ExitCode
+checkoutHakyllBranchSiteTree = shellEmpty "git checkout hakyll _site"
 
+rmImagesTree :: IO ()
 rmImagesTree = rmTree "./images"
+
+rmPostsTree :: IO ()
 rmPostsTree = rmTree "./posts"
+
+rmCssTree :: IO ()
 rmCssTree = rmTree "./css"
 
+rsyncHakyllSiteTree :: IO ExitCode
 rsyncHakyllSiteTree = do
   siteDir <- realpath "./_site"
   cwd <- pwd
@@ -61,17 +84,31 @@ rsyncHakyllSiteTree = do
   let currentPath = format fp cwd
   shellEmpty (Data.Text.concat [(pack "rsync -a "), sitePath, (pack "/ "), currentPath])
 
-pushToMaster = shellEmpty "git add -u && git add -A && git commit --amen && git push -f"
+pushToMaster :: IO ExitCode
+pushToMaster = shellEmpty $ combineCommands [
+    update
+    , commit
+    , push
+  ]
+  where update = "git add -A"
+        commit = "git comit --amen"
+        push = "git push -f"
 
-checkoutHakyll = shellEmpty "git checkout hakyll"
+checkoutHakyllBranch :: IO ExitCode
+checkoutHakyllBranch = shellEmpty "git checkout hakyll"
 
 -- Util
 
+rmTree :: Turtle.FilePath -> IO ()
 rmTree str = do
   exists <- testdir str
-  when exists $
-    do
-      dir <- realpath str
-      rmtree dir
+  when exists $ do
+    dir <- realpath str
+    rmtree dir
 
+shellEmpty :: Text -> IO ExitCode
 shellEmpty str = shell str Turtle.empty
+
+combineCommands :: [Text] -> Text
+combineCommands [] = error "No commands given."
+combineCommands (x:xs) = intercalate " && " $ x : xs
