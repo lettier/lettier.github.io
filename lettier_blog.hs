ce442dacdb92789a16b9ad4b32eb898abcd9889b
Lettier <contact@lettier.com>
2016-11-14 02:46:23 -0500

    Adds and serves bootstrap css and js and fontawesome css due to maxcdn insecure cert.

--- lettier_blog.hs
+++ lettier_blog.hs
@@ -18,6 +18,10 @@ main = hakyll $ do
     route   idRoute
     compile copyFileCompiler
 
+  match "js/*" $ do
+    route   idRoute
+    compile copyFileCompiler
+
   match "css/*" $ do
     route   idRoute
     compile compressCssCompiler
