3bfe5a35f1fc4b6aa52614bf25366bc173c2011f
Lettier <contact@lettier.com>
2016-11-14 21:56:24 -0500

    Revert "Adds and serves bootstrap css and js and fontawesome css due to maxcdn insecure cert."
    This reverts commit ce442dacdb92789a16b9ad4b32eb898abcd9889b.

--- lettier_blog.hs
+++ lettier_blog.hs
@@ -18,10 +18,6 @@ main = hakyll $ do
     route   idRoute
     compile copyFileCompiler
 
-  match "js/*" $ do
-    route   idRoute
-    compile copyFileCompiler
-
   match "css/*" $ do
     route   idRoute
     compile compressCssCompiler
